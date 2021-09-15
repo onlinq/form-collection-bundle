9781baf6b0218f59e3f600ac31b16e2c9e840e68
Tim Goudriaan <tim@onlinq.nl>
2021-09-15 13:20:29 +0200

    Add Symfony Flex integration

---
+++ assets/stimulus_controller.js
@@ -0,0 +1,5 @@
+import { Controller } from 'stimulus';
+
+// We add an empty Stimulus controller since we
+// only use the autoimport feature of Symfony Flex
+export default class extends Controller { }
