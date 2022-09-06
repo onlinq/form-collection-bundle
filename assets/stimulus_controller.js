0009140f4d272094846fdeac9bb270a642e54ed4
Tim Goudriaan <tim@onlinq.nl>
2022-09-06 10:20:23 +0200

    Remove Symfony UX integration

--- assets/stimulus_controller.js
+++
@@ -1,5 +0,0 @@
-import { Controller } from 'stimulus';
-
-// We add an empty Stimulus controller since we
-// only use the autoimport feature of Symfony Flex
-export default class extends Controller { }
